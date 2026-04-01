4561f6c9d0b3c0e0bf3fc3217f96998f28d38f72
Takacs Dániel Zoltán <takacsd117@gmail.com>
2026-04-01 14:10:53 +0200

    F5MJUS - prettier fixes

--- react-projects/src/react-crud/components/genre-legend/genre-legend.jsx
+++ react-projects/src/react-crud/components/genre-legend/genre-legend.jsx
@@ -5,27 +5,30 @@ const GenreLegend = ({ genreColors, activeGenre, onGenreClick }) => {
     <div className="genre-legend-container">
       <div className="genre-legend">
         <div className="legend-header">
-        <span>Szűrés műfaj szerint:</span>
-        {activeGenre && (
-          <button className="clear-filter" onClick={() => onGenreClick(null)}>
-            Összes mutatása
-          </button>
-        )}
-      </div>
-      <div className="legend-items">
-        {Object.entries(genreColors).map(([genre, color]) => (
-          <div 
-            key={genre} 
-            className={`legend-item ${activeGenre === genre ? 'active' : ''} ${activeGenre && activeGenre !== genre ? 'inactive' : ''}`}
-            onClick={() => onGenreClick(genre)}
-          >
-            <span 
-              className="legend-dot" 
-              style={{ backgroundColor: color, boxShadow: `0 0 10px ${color}88` }}
-            ></span>
-            <span className="legend-text">{genre}</span>
-          </div>
-        ))}
+          <span>Szűrés műfaj szerint:</span>
+          {activeGenre && (
+            <button className="clear-filter" onClick={() => onGenreClick(null)}>
+              Összes mutatása
+            </button>
+          )}
+        </div>
+        <div className="legend-items">
+          {Object.entries(genreColors).map(([genre, color]) => (
+            <div
+              key={genre}
+              className={`legend-item ${activeGenre === genre ? 'active' : ''} ${activeGenre && activeGenre !== genre ? 'inactive' : ''}`}
+              onClick={() => onGenreClick(genre)}
+            >
+              <span
+                className="legend-dot"
+                style={{
+                  backgroundColor: color,
+                  boxShadow: `0 0 10px ${color}88`,
+                }}
+              ></span>
+              <span className="legend-text">{genre}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
