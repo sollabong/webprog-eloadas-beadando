3dba289cce606f2f3b097dd7e40ac1009695aa5b
Sipos Gábor János <spkone7@gmail.com>
2026-04-02 14:03:18 +0200

    prettier fixes

--- react-projects/src/shared-components/header/header.jsx
+++ react-projects/src/shared-components/header/header.jsx
@@ -4,13 +4,27 @@ const Header = () => {
       <h1>Web programozás-1 Előadás Házi feladat</h1>
       <nav>
         <ul>
-          <li><a href="/index.html">Főoldal</a></li>
-          <li><a href="/javascript-crud/javascript.html">Javascript CRUD</a></li>
-          <li><a href="/react.html">React CRUD</a></li>
-          <li><a href="/spa.html">SPA</a></li>
-          <li><a href="/fetchapi/fetchapi.html">Fetch API</a></li>
-          <li><a href="/axios.html">Axios CRUD</a></li>
-          <li><a href="/oojs/oojs.html">OOJS App</a></li>
+          <li>
+            <a href="/index.html">Főoldal</a>
+          </li>
+          <li>
+            <a href="/javascript-crud/javascript.html">Javascript CRUD</a>
+          </li>
+          <li>
+            <a href="/react.html">React CRUD</a>
+          </li>
+          <li>
+            <a href="/spa.html">SPA</a>
+          </li>
+          <li>
+            <a href="/fetchapi/fetchapi.html">Fetch API</a>
+          </li>
+          <li>
+            <a href="/axios.html">Axios CRUD</a>
+          </li>
+          <li>
+            <a href="/oojs/oojs.html">OOJS App</a>
+          </li>
         </ul>
       </nav>
     </header>
