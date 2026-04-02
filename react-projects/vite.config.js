7276152683ca393249022060932c665b7a8e3f9e
Takacs Dániel Zoltán <takacsd117@gmail.com>
2026-04-02 14:03:18 +0200

    prettier fixes

--- react-projects/vite.config.js
+++ react-projects/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '../', 
+  root: '../',
   server: {
     open: '/index.html',
     fs: {
-      allow: ['../'], 
+      allow: ['../'],
     },
   },
   build: {
