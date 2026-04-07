40cc40daf3cfc0495384b3d434afbbdfd2a37978
Sipos Gábor János <spkone7@gmail.com>
2026-04-07 13:19:33 +0200

    RUG70U - task 4 - two cards added for the two apps - TODO: add path and descr to apps

---
+++ react-projects/src/spa/components/spa-container/spa-container.jsx
@@ -0,0 +1,47 @@
+import './spa-container.css';
+
+const SpaContainer = () => {
+  return (
+    <div className="spa-page-container">
+        <section className="spa-container">
+        <h2>
+            <i className="fas fa-database"></i> Single Page App - React Apps
+        </h2>
+        <div className="spa-apps-container">
+            <div className='app-card'>
+            <div className='app-card-header'>
+                <i className="fas fa-cloud"></i>
+                <h3>First App</h3>
+            </div>
+            <div className='app-card-content'>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+            <button type="button" className="btn btn-save">
+                Megnézem
+            </button>
+            </div>
+            <div className='app-card'>
+            <div className='app-card-header'>
+                <i className="fas fa-tree"></i>
+                <h3>Second App</h3>
+            </div>
+            <div className='app-card-content'>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+            <button type="button" className="btn btn-save">
+                Megnézem
+            </button>
+            </div>
+        </div>
+        </section>
+    </div>
+  );
+};
+
+export default SpaContainer;
