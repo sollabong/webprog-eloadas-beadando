0921d86ec7a38e0d283fba655e2db73a3f39ec93
Sipos Gábor János <spkone7@gmail.com>
2026-04-09 16:15:45 +0200

    RUG70U - task 7 - grid element class created as a base for other snake elements

---
+++ oojs/js/grid-element.js
@@ -0,0 +1,19 @@
+class GridElement {
+    constructor(x, y, className) {
+        this.x = x;
+        this.y = y;
+        this.element = document.createElement('div');
+        this.element.className = 'dot ' + className;
+        document.getElementById('game-board').appendChild(this.element);
+        this.draw();
+    }
+
+    draw() {
+        this.element.style.left = this.x + 'px';
+        this.element.style.top = this.y + 'px';
+    }
+
+    remove() {
+        this.element.remove();
+    }
+}
