9fa47a9b329971ea8718e0f0a8f804088fd06928
Sipos Gábor János <spkone7@gmail.com>
2026-04-17 14:06:06 +0200

    skgj deleted :(

--- react/src/shared-components/footer/footer.jsx
+++ react/src/shared-components/footer/footer.jsx
@@ -9,9 +9,6 @@ const Footer = () => {
             Sipos Gábor János <span className="footer-neptun"> RUG70U</span>
           </span>
           <span className="footer-name">
-            Soltész-Kéri Gábor <span className="footer-neptun"> D0345Y</span>
-          </span>
-          <span className="footer-name">
             Takács Dániel Zoltán <span className="footer-neptun"> F5MJUS</span>
           </span>
         </div>
